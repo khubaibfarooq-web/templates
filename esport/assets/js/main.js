e1f0e8d6b4469969766a2dc1725322fae3b25025
Khubaib Butt <khubaibfarooq1994@gmail.com>
2022-02-25 02:24:37 +0500

    esport webview app

---
+++ esport/assets/js/main.js
@@ -0,0 +1,12 @@
+
+// Top Matches Carousel
+$('#matches').owlCarousel({
+		items:1,
+		loop:true,
+		margin:20,
+		dots : true,
+		nav: false,
+		autoplay : true,
+		autoWidth:true,
+		// stagePadding: 100
+	});
